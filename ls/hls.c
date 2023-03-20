4512d070e2b0deffe91f22648031cbfecadce885
HINKOKO <piotr62@live.fr>
2023-03-20 17:22:34 +0100

    first version

--- ls/hls.c
+++ ls/hls.c
@@ -6,28 +6,44 @@
 
 /**
 * main - sample `ls` command
-* @argc: number of arguments passed to program
-* @argv: array of strings-arguments
+* @argc: integer, number of command line arguments
+* @argv: array of strings, contains command line arguments
+* passed to the program
 * Return: 0 for success, 1 for error
 */
 
 
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
-	struct dirent *entry;
 	DIR *dir;
+	struct dirent *dp;
 
-	if (argc == 1)
+	if (argc < 2)
 	{
+		/* no directory specified, use the current one*/
 		dir = opendir(".");
-		while ((entry = readdir(dir)) != NULL)
-		{
-			if (entry->d_name[0] == '.')
-				continue;
-			printf("%s\n", entry->d_name);
-		}
+	}
+	else
+	{
+		/* use the specified directory */
+		dir = opendir(argv[1]);
 	}
 
+	if (dir == NULL)
+	{
+		/* unable to open dir  */
+		perror("opendir");
+		return (1);
+	}
+
+	while ((dp = readdir(dir)) != NULL)
+	{
+		/* continue if hidden files */
+		if (dp->d_name[0] == '.')
+			continue;
+		printf("%s\n", dp->d_name);
+	}
 	closedir(dir);
+
 	return (0);
 }
