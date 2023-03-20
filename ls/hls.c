4f19ef0d4585386b36f13a5a976a0bd0e124b0e8
HINKOKO <piotr62@live.fr>
2023-03-20 14:33:28 +0100

    testing another way

--- ls/hls.c
+++ ls/hls.c
@@ -12,33 +12,38 @@
 * Return: 0 for success, 1 for error
 */
 
+
 int main(int argc, char *argv[])
 {
-DIR *dir;
-struct dirent *entry;
+	DIR *dir;
+	struct dirent *dp;
 
-if (argc < 2)
-{
-	fprintf(stderr, "Usage: %s directory_name\n", argv[0]);
-	exit(EXIT_FAILURE);
-}
+	if (argc < 2)
+	{
+		/* no directory specified, use the current one*/
+		dir = opendir(".");
+	}
+	else
+	{
+		/* use the specified directory */
+		dir = opendir(argv[1]);
+	}
 
-dir = opendir(argv[1]);
+	if (dir == NULL)
+	{
+		/* unable to open dir  */
+		perror("opendir");
+		return (1);
+	}
 
-if (dir == NULL)
-{
-	perror("opendir");
-	exit(EXIT_FAILURE);
-}
+	while ((dp = readdir(dir)) != NULL)
+	{
+		/* continue if hidden files */
+		if (dp->d_name[0] == '.')
+			continue;
+		printf("%s\n", dp->d_name);
+	}
+	closedir(dir);
 
-while ((entry = readdir(dir)) != NULL)
-{
-	if (entry->d_name[0] == '.')
-		continue;
-	printf("%s\n", entry->d_name);
-}
-
-closedir(dir);
-
-return (0);
+	return (0);
 }
