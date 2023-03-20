f2acc906117782bba9cbc45aa658ab3afd37b3a4
HINKOKO <piotr62@live.fr>
2023-03-20 13:52:43 +0100

    testing a more advanced code

--- ls/hls.c
+++ ls/hls.c
@@ -1,49 +1,84 @@
 #include <stdio.h>
+#include <string.h>
 #include <dirent.h>
 #include <stddef.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 /**
-* main - sample `ls` command
-* @argc: integer, number of command line arguments
-* @argv: array of strings, contains command line arguments
-* passed to the program
-* Return: 0 for success, 1 for error
+ * sample_ls - core process of ls implementation
+ * @dir: the dir to open and read
+ * @op_a: for option 1
+ * @op_l: for option 2
 */
 
+void sample_ls(const char *dir, int op_a, int op_l)
+{
+	struct dirent *d;
+	DIR *dh = opendir(dir);
+
+	if (!dh)
+	{
+		if (errno == ENOENT)
+		{
+			perror("Directory do not exists");
+		}
+		else
+			perror("Unable to read directory");
+	exit(EXIT_FAILURE);
+	}
+
+	while ((d = readdir(dh)) != NULL)
+	{
+		if (!op_a && d->d_name[0] == '.')
+			continue;
+		printf("%s ", d->d_name);
+		if (op_l)
+			printf("\n");
+	}
+
+	if (!op_l)
+		printf("\n");
+}
+
+/**
+* main - main function
+* @argc: number of arguments passed to main
+* @argv: array of strings of argument
+* Return: 0 success, 1 failure
+*/
 
 int main(int argc, char *argv[])
 {
-	DIR *dir;
-	struct dirent *dp;
-
-	if (argc < 2)
+	if (argc == 1)
 	{
-		/* no directory specified, use the current one*/
-		dir = opendir(".");
+		sample_ls(".", 0, 0);
 	}
-	else
+	else if (argc == 2)
 	{
-		/* use the specified directory */
-		dir = opendir(argv[1]);
-	}
+		if (argv[1][0] == '-')
+		{
+			int op_a = 0, op_l = 0;
+			char *p = (char *)(argv[1] + 1);
 
-	if (dir == NULL)
-	{
-		/* unable to open dir  */
-		perror("opendir");
-		return (1);
+			while (*p)
+			{
+				if (*p == 'a')
+					op_a = 1;
+				else if (*p == 'l')
+					op_l = 1;
+				else
+				{
+					perror("Option unavailable");
+					exit(EXIT_FAILURE);
+				}
+				p++;
+			}
+			sample_ls(".", op_a, op_l);
+		}
 	}
-
-	while ((dp = readdir(dir)) != NULL)
-	{
-		/* continue if hidden files */
-		if (dp->d_name[0] == '.')
-			continue;
-		printf("%s\n", dp->d_name);
-	}
-	closedir(dir);
-
 	return (0);
 }
