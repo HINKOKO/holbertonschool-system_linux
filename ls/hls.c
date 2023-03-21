a3859b186bc827e170b993fe68441166df6a53d3
HINKOKO <piotr62@live.fr>
2023-03-21 15:35:50 +0100

    got you I think

--- ls/hls.c
+++ ls/hls.c
@@ -21,11 +21,11 @@ void list_dir(const char *dirname, const char *path, int display_dirname)
 	if (dir == NULL)
 	{
 		if (errno == ENOENT)
-		{
-		fprintf(stderr, "./hls_01: cannot access directory %s: ", path);
+			fprintf(stderr, "./hls_01: cannot access directory %s: ", path);
+		else if (errno == EACCES)
+			fprintf(stderr, "./hls_01: cannot open directory %s: ", path);
 		perror("");
 		return;
-		}
 	}
 	if (display_dirname)
 		printf("%s:\n", dirname);
