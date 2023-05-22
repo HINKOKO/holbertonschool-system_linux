bb9f7960297b032e9c8bc86266b6e024e4864acb
HINKOKO <piotr62@live.fr>
2023-05-23 00:36:43 +0200

    another version pointilleuse

--- libasm/2-strncmp.asm
+++ libasm/2-strncmp.asm
@@ -1,50 +1,73 @@
-BITS 64
-	global	asm_strncmp
+bits 64
+
+section .text
+global asm_strncmp ; exporting the function
 
 asm_strncmp:
-	; registers in order rdi, rsi, rdx, rcx, r8, r9
-	push rbp
-	mov rbp, rsp ; setup
-	cmp rdx, 0 ; argv[3] the size to compare to in rdx
-	je size_zero
-	cmp rdi, 0
-	je end
-	cmp rsi, 0
-	je end
-	xor rcx, rcx ; trick xor to initialize counter to 0 fast
+	push rbp ; prologue
+	mov rbp, rsp
+	push rbx
+	push rcx
+	push r11
+	push r12
+	push r13
 
-loop:
-	mov al, [rdi + rcx] ; get the first character of 1st string
-	mov r8b, [rsi + rcx] ; get the first character of 2nd string
-	cmp al, 0 ; check for end of string 1
-	je compare
-	cmp r8b, 0 ; check for end of string 2
-	je compare
-	cmp al, r8b
-	jne compare
-	inc rcx
-	cmp rcx, rdx ; compare counter to parameter size_t n asked by user
-	je compare
-	jmp loop
+	mov rbx, rdi ; save 1st arg in rbx register
+	mov rcx, rsi ; save 2nd arg in rcx register
+	mov r11, rdx ; save 3rd arg in r11 register
+	mov r12, 0 ; set a counter to 0
 
-compare:
-	sub al, r8b ; al -= r8b
-	movsx rax, al ; move and get the sign please
-	jmp end
+count_loop:
+	inc r12 ; increment counter
+	cmp r12, r11 ; check if count greater than n bytes parameter passed to strncmp function
+	jg correction ; if yes go to correction and decrease back count
+	mov r13b, byte [rcx + r12 - 1]
+	cmp r13b, byte 0x00 ; check if not a null char in arg1
+	je next_loop ; if yeah get the hell out and jump next loop
+	cmp r13b, byte [rbx + r12 - 1] ; check if not a null char in arg2
+	je next_loop
+	cmp r13b, byte [rcx + r12 - 1] ; compare char from arg1 & arg2
+	je count_loop ; Repeat the loop
 
-greater:
-	mov rax, 1
-	jmp end
+correction:
+	dec r12;
+	jmp next_loop
+
+next_loop:
+	mov rax, 0 ; init return value 0
+	push qword [rbx + r12 - 1] ; put the last char arg1 compared onto stack
+	push qword [rcx + r12 - 1] ; put the last char arg2 compared onto stack
+	mov rbx, 0 ; set register to 0
+	mov rcx, 0 ; set register to 0
+	mov bl, byte [rsp + 8] ; move the last char from arg1 in a variable
+	mov cl, byte [rsp] ; move the last char from arg2 in a variable
+	cmp rbx, rcx ; compare the two previous buddies
+	je equal
+	jl less
+	jg greater
+
+
+equal:
+	mov rax, 0 ; set return val to 0
+	jmp end ; jump to end of program
 
 less:
-	mov rax, -1
-	jmp end
+	mov rax, -1 ; set return value to -1
+	jmp end ; jump to end
 
-size_zero:
-	mov rax, 0
-	jmp end
+greater:
+	mov rax, 1 ; set return value to 1
+	jmp end ; jump to end
 
 end:
+	pop rdx
+	pop rcx
+	pop r13
+	pop r12
+	pop r11
+	pop rcx
+	pop rbx ; restore register backwards
 	mov rsp, rbp
-	pop rbp
+	pop rbp ; epilogue of stack frame
 	ret
+
