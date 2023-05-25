9449787b8a00ef594195775acfb3d762312b7c46
HINKOKO <piotr62@live.fr>
2023-05-25 12:30:23 +0200

    fixin betty and remove push/pop intermediate step

--- libasm/7-puts.asm
+++ libasm/7-puts.asm
@@ -13,15 +13,12 @@ asm_puts:
 
 	push rdi
 	call asm_strlen
-	; pop rdi
-
-	; push rdi
 	push rsi
 	push rdx
 	mov rdx, rax ; bytes written in rdx count
 	; ==> whenever we call a function or syscall, rax hold their return value
 	; thus, let's move rax to rdx => the data register for I/O OPS
-	mov rax, 1 ; syscall write 1 
+	mov rax, 1 ; syscall write 1
 	mov rsi, rdi ;
 	mov rdi, 1 ; fd 1 stdout to dest instruction I/O
 	syscall
