8ac100dfd7f1dda88b94aaba2ef974f8a6b6267c
HINKOKO <piotr62@live.fr>
2023-05-25 12:21:49 +0200

    Task 7 seems ok

---
+++ libasm/7-puts.asm
@@ -0,0 +1,34 @@
+bits 64
+
+	global asm_puts ; export the function
+	extern asm_strlen ; import strlen
+
+	section .text
+
+	; prints a string of chars on stdout
+
+asm_puts:
+	push rbp;
+	mov rbp, rsp ; setup stack frame
+
+	push rdi
+	call asm_strlen
+	pop rdi
+
+	push rdi
+	push rsi
+	push rdx
+	mov rdx, rax ; bytes written in rdx count
+	mov rax, 1 ; syscall write 1
+	mov rsi, rdi ;
+	mov rdi, 1 ; fd 1 stdout to dest instruction I/O
+	syscall
+
+	pop rdx
+	pop rsi
+	pop rdi
+	mov rsp, rbp
+	pop rbp
+	ret
+
+
