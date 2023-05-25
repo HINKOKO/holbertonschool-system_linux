79b2aaf003692a549062c124b58b08f70cec9037
HINKOKO <piotr62@live.fr>
2023-05-25 22:40:00 +0200

    adding comments in code

--- libasm/8-strcasecmp.asm
+++ libasm/8-strcasecmp.asm
@@ -35,33 +35,33 @@ asm_strcasecmp:
 mainloop:
 	mov r8b, [rdi + rcx] ; load one byte of str1
 	mov r9b, [rsi + rcx] ; load one byte of str2
-	cmp r8b, 0x41
-	jl second_string
-	cmp r8b, 0x5A
-	jg second_string
-	add r8b, 0x20
+	cmp r8b, 0x41 ; compare with A value hex (man 7 ascii)
+	jl second_string ; if less jump to second string handling
+	cmp r8b, 0x5A ; compare with Z value hex
+	jg second_string ; if greater jump to second string handling
+	add r8b, 0x20 ; if 0x41 | r8b | 0x5A => +=32 to convert
 
 second_string:
-	cmp r9b, 0x41
-	jl compare
-	cmp r9b, 0x5A
-	jg compare
-	add r9b, 0x20
+	cmp r9b, 0x41 ; same stuff as string 1
+	jl compare ; if not upper too, lets make a contest between !
+	cmp r9b, 0x5A ; same stuff as string 1
+	jg compare ; if not upper too, lets make a contest !
+	add r9b, 0x20 ; if upper += 32 to convert
 
 compare:
-	movzx eax, r8b
-	sub eax, r9d
-	test eax, eax
+	movzx eax, r8b ; movzx to 'zero extend' when we come from lower to bigger register to keep safe value
+	sub eax, r9d ; r9'd' to 'match' the 32 bits of eax
+	test eax, eax ; if not 0 jump to end
 	jnz end
-	test r8b, r8b
-	jz end
-	inc rcx
-	jmp mainloop
+	test r8b, r8b ; did we reach end of string1
+	jz end ; if yes go to end
+	inc rcx ; increment iterator
+	jmp mainloop ; back to mainloop to repeat
 
 end:
 	pop r9
 	pop r8
-	pop rcx
+	pop rcx ; restore the registers we pushed
 
 	mov rbp, rsp
 	pop rbp
