89b77b93d29d4852e68ee3fdce9b8d052b402828
HINKOKO <piotr62@live.fr>
2023-05-25 10:59:25 +0200

    keepin register with byte count

--- libasm/6-putc.asm
+++ libasm/6-putc.asm
@@ -11,10 +11,6 @@ asm_putc:
 	push rbp
 	mov rbp, rsp ; stack frame set_up (ENTER)
 
-	push rsi
-	push rdi
-	push rdx
-	push rax
 
 	mov rax, 1 ; syscall write
 	mov rdi, 1 ; write the char
@@ -22,11 +18,6 @@ asm_putc:
 	mov rdx, 1 ; count the byte
 
 	syscall ; perform the system call to write
-
-	pop rax
-	pop rdx
-	pop rdi
-	pop rsi ; restore registers
 
 	mov rsp, rbp
 	pop rbp ; restore stack frame
