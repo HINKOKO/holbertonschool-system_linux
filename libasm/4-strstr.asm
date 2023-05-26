56e7e457c4cab875da921b959c077f4302b4a04f
HINKOKO <piotr62@live.fr>
2023-05-26 14:22:17 +0200

    improving/clearer

--- libasm/4-strstr.asm
+++ libasm/4-strstr.asm
@@ -5,6 +5,8 @@ bits 64
 	section .text
 	; asm_strstr is copycat of C function strstr
 	; locates a subtring 'needle' in the 'haystack' of a mega huge (or not) string
+	; strstr => finds the first occurence of 'needle' in 'haystack' ('\0' not compared)
+	; returns a pointer to begin of located substring , NULL not found
 
 
 asm_strstr:
@@ -17,23 +19,26 @@ asm_strstr:
 	jmp boot ; jump to boot
 
 boot:
-	mov r9b, [rdi] ; (set) then reset the r9 register with 1st arg
+	mov r8b, [rdi] ; set/reset r8b with str[i] at updated position to continue comparison
 	xor rcx, rcx ; clear the counter
 	jmp while ; jump to the 'core' while loop
 
-reboot_count:
-	cmp r9b, 0 ; check if str1[i] != '\0'
-	je no_equal ; if yes jump to no equal (str2 not finished but str1 yes => problem)
+reboot_comp:
+	cmp r8b, 0 ; check if str1[i] != '\0'
+	je no_equal ; if yes jump to no equal (str2 not finished but str1 yes => problem needle bigger than haystack)
 	inc rdi ; str1[i++] move the iterator one step
 	jmp boot
 
 while:
-	mov r9b, [rdi + rcx] ; temp r9b now hosting str1[i]
-	mov r8b, [rsi + rcx] ; temp r8b hosting str2[i]
-	cmp r8b, 0 ; did we reach end of needle string (str2) ?
+	mov r8b, [rdi + rcx] ; temp r9b now hosting str1[i]
+	mov r9b, [rsi + rcx] ; temp r8b hosting str2[i]
+	cmp r9b, 0 ; did we reach end of needle string (str2) ?
+	; step is strange but we will never be here if str2 is NULL
+	; just to ensure if needle is one char (that's a needle really)
+	; and find boom occurs
 	je strings_equal ; if yes jump to string_equals
-	cmp r9b, r8b ; compare str1[i] & str2[i]
-	jne reboot_count ; if no jump to init_val
+	cmp r8b, r9b ; compare str1[i] & str2[i]
+	jne reboot_comp ; if no jump to init_val
 	inc rcx ; move our counter += 1
 	jmp while ; Repeat the loop
 
