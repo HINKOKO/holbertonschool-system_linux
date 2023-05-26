c03d6b3408688b2c461b658a42981b8305b72cfa
HINKOKO <piotr62@live.fr>
2023-05-26 14:58:51 +0200

    better docs , and practice a test with checker

--- libasm/6-putc.asm
+++ libasm/6-putc.asm
@@ -7,15 +7,20 @@ bits 64
 	; size_t asm_putc(int c);
 	; prints a single character on the stdout
 
+	; write syscall expects the following args
+	; => rax (syscall number) set to 1
+	; => rdi (file descriptor) set to 1 for stdout
+	; => rsi (buffer pointer) set to address of character to be written
+	; => rdx (buffer size) set to 1 , to specify hey we write a single char (1 byte)
+
 asm_putc:
 	push rbp
 	mov rbp, rsp ; stack frame set_up (ENTER)
 
-	push rdi ; save register if were in use
 
 	mov rax, 1 ; syscall write
 	mov rdi, 1 ; write the char
-	mov rsi, rsp ; source pointer to address to stack
+	mov rsi, rsp ; set to the address of character to be written
 	mov rdx, 1 ; count the char written
 	syscall ; perform the system call to write
 
