bdaeced3e0f7a494a4f3514e7ace9ed47809682d
HINKOKO <piotr62@live.fr>
2023-05-26 15:11:57 +0200

    Task 7 better docs

--- libasm/7-puts.asm
+++ libasm/7-puts.asm
@@ -13,13 +13,13 @@ asm_puts:
 
 	push rdi
 	call asm_strlen
-	push rsi
+	push rsi ; rsi , source index, used as a pointer to a source in stream ops
 	push rdx
 	mov rdx, rax ; bytes written in rdx count
 	; ==> whenever we call a function or syscall, rax hold their return value
 	; thus, let's move rax to rdx => the data register for I/O OPS
 	mov rax, 1 ; syscall write 1
-	mov rsi, rdi ;
+	mov rsi, rdi ; set rsi to the address of string to be written
 	mov rdi, 1 ; fd 1 stdout to dest instruction I/O
 	syscall
 
