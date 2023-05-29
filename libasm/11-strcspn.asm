f08723b6bcb9b8b8278aef602cbd8961e60856a3
HINKOKO <piotr62@live.fr>
2023-05-29 10:42:11 +0200

    Testting 11

--- libasm/11-strcspn.asm
+++ libasm/11-strcspn.asm
@@ -10,38 +10,26 @@ asm_strcspn:
 	push rbp
 	mov rbp, rsp ; A beautiful prologue
 
-	push rcx
-	push r8
-	push r9 ; save those babies
-
 	xor rax, rax ; clear accumulator
-	xor r8, r8
-	xor r9, r9
-
-loop_s:
-	mov r8b, [rdi + rcx]
-	test r8b, r8b
-	jz end
 	xor rcx, rcx
 
-loop_reject:
+loop:
+	mov r8b, [rdi + rcx]
 	mov r9b, [rsi + rcx]
-	test r9b, r9b
+	test r8b, r8b
 	jz end
+	test r9b, r9b
+	jz bingo
 	cmp r8b, r9b
-	jne increment
+	je end
 	inc rcx
-	jmp loop_reject
+	jmp loop
 
-increment:
+bingo:
 	inc rax
-	jmp loop_s
+	jmp loop
 
 end:
-	pop r9
-	pop r8
-	pop rcx ; restore the registers
-
 	mov rsp, rbp
 	pop rbp
 	ret
