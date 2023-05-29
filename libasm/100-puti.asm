bf48e57fa9b246c1ac80e37c769892fcfa0b3b98
HINKOKO <piotr62@live.fr>
2023-05-29 18:15:29 +0200

    work in progress

--- libasm/100-puti.asm
+++ libasm/100-puti.asm
@@ -25,15 +25,36 @@ asm_puti:
 
 	cmp r8, 0
 	jge positive
-
-
+	imul r8, -1
+	mov rdi, 45
+	call asm_putc
 
 positive:
 	test r8, r8
 	jnz convert_loop
 	mov rdi, 48
 	call
-	
+
+convert_loop:
+	xor rdx, rdx
+	test r8, r8 ; end of 'n' number ? go to next
+	jz next
+	mov rax, r8
+	mov rbx, 10 ; base 10 to divide
+	div rbx ; rax /= rbx
+
+	mov r8, rax ; quotient in rax
+	push rdx ; remainder in rdx
+
+	inc r9 ; count the bytes written
+	jmp convert_loop
+
+next:
+
+print_loop:
+	test r8, r8
+	jz whatever ; ???
+
 end:
 	pop r10
 	pop r9
