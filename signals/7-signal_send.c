bea9c5f0820477cc5c582c5567f61e00012480b9
HINKOKO <piotr62@live.fr>
2023-06-08 10:26:37 +0200

    Fixing typo detail on stderr message

--- signals/7-signal_send.c
+++ signals/7-signal_send.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 {
 	if (argc < 2)
 	{
-		fprintf(stderr, "Usage %s <pid>", argv[0]);
+		fprintf(stderr, "Usage: %s <pid>", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 	return (kill(atoi(argv[1]), SIGINT) == -1 ? EXIT_FAILURE : EXIT_SUCCESS);
