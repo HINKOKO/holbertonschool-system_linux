c56754be21e8abbfbf7e3dc81077dafa8ec4ff80
HINKOKO <piotr62@live.fr>
2023-06-08 15:16:41 +0200

    updating headers

--- signals/signals.h
+++ signals/signals.h
@@ -5,11 +5,13 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <stddef.h>
 
 int handle_signal(void);
 void (*current_handler_signal(void))(int);
 int handle_sigaction(void);
 void (*current_handler_sigaction(void))(int);
 int trace_signal_sender(void);
+int pid_exist(pid_t pid);
 
 #endif /* __SIGNALISTICS__*/
