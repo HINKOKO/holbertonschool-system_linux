34b2894ae4eed1b1a4383f82beaad2f2d54fd8ad
HINKOKO <piotr62@live.fr>
2023-06-08 10:25:31 +0200

    Task 7 test

---
+++ signals/7-signal_send.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+
+/**
+ * main - send the signal SIGINT to a process
+ * given its PID (using kill(pid, signal))
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		fprintf(stderr, "Usage %s <pid>", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	return (kill(atoi(argv[1]), SIGINT) == -1 ? EXIT_FAILURE : EXIT_SUCCESS);
+}
