99c15a6c3428f3d932e9f9e7013f7f88f9646b0f
HINKOKO <piotr62@live.fr>
2023-06-25 14:50:02 +0200

    header file

---
+++ nm_objdump/hnm.h
@@ -0,0 +1,13 @@
+#ifndef __HNM__
+#define __HNM__
+
+#include <stdio.h>
+#include <elf.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+#endif /* __HNM__ */
