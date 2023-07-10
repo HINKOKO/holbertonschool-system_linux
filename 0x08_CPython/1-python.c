158b33906f2d0cc3d0718f18412477e8fa13bfe6
HINKOKO <piotr62@live.fr>
2023-07-10 13:02:28 +0200

    obfuscus documentation

--- 0x08_CPython/1-python.c
+++ 0x08_CPython/1-python.c
@@ -12,7 +12,7 @@ void print_python_list(PyObject *p)
 {
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python list = %zd\n", Py_SIZE(p));
-	printf("[*] Allocated = %zd\n", (PyListObject *)p->allocated);
+	printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
 	for (int i = 0; i < PyList_Size(p); i++)
-		printf("Element %d: %s\n", i, Py_TYPE(PyList_GET_ITEM(p, i)->ob_type));
+		printf("Element %d: %s\n", i, Py_TYPE(PyList_GET_ITEM(p, i))->tp_name);
 }
