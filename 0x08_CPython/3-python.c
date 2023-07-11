3b893d2a3385ff5c34354ffdd747abb0dd336fd2
HINKOKO <piotr62@live.fr>
2023-07-11 11:12:37 +0200

    lauching task 3

---
+++ 0x08_CPython/3-python.c
@@ -0,0 +1,94 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+#include <bytesobject.h>
+#include <floatobject.h>
+#include <stdlib.h>
+
+void print_python_bytes(PyObject *p);
+
+/**
+ * print_python_float - print basic info about Python Float object
+ * @p: pointer to Python object
+*/
+
+void print_python_float(PyObject *p)
+{
+	PyFloatObject *f = (PyFloatObject *)p;
+	double val = f->ob_fval;
+	char *str = NULL;
+
+	printf("[.] float object info\n");
+	if (!PyFloat_Check(f))
+	{
+		printf("  [ERROR] Invalid Float Object\n");
+		return;
+	}
+	str = PyOS_double_to_string(val, 'r', 0, Py_DTSF_ADD_DOT_0, NULL);
+	printf("  value: %s\n", str);
+}
+
+/**
+ * print_python_bytes - print basic info about python byte object
+ * @p: pointer to PyObject ( a byte one hence )
+*/
+
+void print_python_bytes(PyObject *p)
+{
+	char *str = NULL;
+	Py_ssize_t size;
+
+	printf("[.] bytes object info\n");
+
+	if (!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+
+	PyBytes_AsStringAndSize(p, &str, &size);
+	printf("  size: %zd\n", size);
+	printf("  trying string: %s\n", str);
+	printf("  first %zd bytes:", size <= 10 ? size + 1 : 10);
+
+	for (int i = 0; i <= size && i < 10; i++)
+	{
+		printf(" %02hhx", str[i]);
+	}
+	printf("\n");
+}
+
+/**
+ * print_python_list - prints basic info about given
+ * Python lists
+ * @p: PyObject object
+*/
+
+void print_python_list(PyObject *p)
+{
+	long int size;
+	PyListObject *list = (PyListObject *)p;
+	const char *type;
+
+	printf("[*] Python list info\n");
+	if (!PyList_Check(list))
+	{
+		printf("  [ERROR] Invalid List Object\n");
+		return;
+	}
+	size = ((PyVarObject *)p)->ob_size;
+
+	printf("[*] Size of the Python List = %zd\n", size);
+	printf("[*] Allocated = %zd\n", list->allocated);
+	for (int i = 0; i < size; i++)
+	{
+		type = (list->ob_item[i])->ob_type->tp_name;
+		printf("Element %d: %s\n", i, type);
+		if (!strcmp(type, "bytes"))
+			print_python_bytes(list->ob_item[i]);
+		if (!strcmp(type, "float"))
+			print_python_float(list->ob_item[i]);
+	}
+}
+
+
