8dd604a947f39d377a41a37ce6870c2c7a2383b0
HINKOKO <piotr62@live.fr>
2023-07-21 21:58:45 +0200

    refacto as PLD and add comments for future generations

--- 0x08_CPython/100-python.c
+++ 0x08_CPython/100-python.c
@@ -1,36 +1,137 @@
 #include <Python.h>
-#include <stdbool.h>
 
-#define PYABS(x) ((x) < 0 ? (-x) : (x))
+char *decimal_to_string(digit *ptr_out, ssize_t size, int neg);
+int long_to_str(PyObject *lg, char **str);
 
 /**
  * print_python_int - display a PyInt Object
  * @p: generic pointer to PyObject
+ *
+ * _PyLong_DECIMAL_SHIFT => used to convert between Python's
+ * internal binary repr
+ * && the decimal repr used in the 'decimal' module
  */
 
 void print_python_int(PyObject *p)
 {
-	Py_ssize_t py_size, i, absolut;
-	unsigned long cout = 0;
-	digit *py_digits;
+	char *str = NULL;
 
-	if (!PyLong_Check(p))
+	if (long_to_str(p, &str) == -1)
 	{
-		printf("Invalid Int Object\n");
+		printf("PROBLEM DUDE\n");
 		return;
 	}
+	printf("%s\n", str);
+	free(str);
+}
 
-	py_size = ((PyVarObject *)p)->ob_size, absolut = PYABS(py_size);
-	py_digits = ((PyLongObject *)p)->ob_digit;
+/**
+ * long_to_str - extract the digit from PyLong object
+ * to decimals digits, then to be feeded to next function
+ * @lg: generic pointer to PyObject
+ * @str: double pointer to str
+ * Return: -1 if failure, or 0 after successfull completion
+ */
 
-	if (py_size > 3 || (py_size == 3 && py_digits[2] > 15))
+int long_to_str(PyObject *lg, char **str)
+{
+	PyLongObject *py;
+	ssize_t size, size_py, i, j;
+	digit *ptr_out, *ptr_in;
+	int neg, d_num;
+
+	py = (PyLongObject *)lg;
+	size_py = Py_SIZE(lg) < 0 ? -Py_SIZE(lg) : Py_SIZE(lg);
+	neg = size_py < 0;
+
+	/**
+	 * To determine the number of decimal digits required
+	 * to represent the Python long integer value as string in the console
+	 * extremely crucial since J.Barbier wants us to overflow the capacity of
+	 * our beloved C programming language, evil guy
+	 * 2**30 is equal to = 1,073,741,824 ~ 1 billion (10^9)
+	 * Thats why _PyLong_DECIMAL_SHIFT 9, by shifting
+	 * 9 position assuring accurate to decimal ?
+	 * d_num = (33*_PyLong_DECIMAL_SHIFT) /
+	 * (10*PyLong_SHIFT - 33 *_PyLong_DECIMAL_SHIFT)
+	 * GUESS WHAT THIS FANCY BADABOUM MAKES 99
+	*/
+	d_num = 99;
+	printf("d_num is tada ! \t%i\n", d_num);
+	size = 1 + size_py + size_py / d_num;
+	ptr_out = calloc(sizeof(digit), size);
+	if (!ptr_out)
+		return (-1);
+	ptr_in = py->ob_digit;
+	size = 0;
+	for (i = size; --i >= 0;)
 	{
-		printf("C unsigned long int overflow\n");
-		return;
+		digit hi = ptr_in[i];
+
+		for (j = 0; j < size; j++)
+		{
+			twodigits z = (twodigits)ptr_out[j] << PyLong_SHIFT | hi;
+
+			hi = (digit)(z / _PyLong_DECIMAL_BASE);
+			ptr_out[j] = (digit)(z - (twodigits)hi * _PyLong_DECIMAL_BASE);
+		}
+		while (hi)
+		{
+			ptr_out[size++] = hi % _PyLong_DECIMAL_BASE;
+			hi /= _PyLong_DECIMAL_BASE;
+		}
 	}
-	for (i = 0; i < absolut; i++)
-		cout += py_digits[i] * (1L << (PyLong_SHIFT * i));
-	if (py_size < 0)
-		printf("-");
-	printf("%lu\n", cout);
+	if (size == 0)
+		ptr_out[size++] = 0;
+	*str = decimal_to_string(ptr_out, size, neg);
+	return (0);
+}
+
+/**
+ * decimal_to_string - now takes the digit decimal
+ * and throw it to a *str
+ * @ptr_out: pointer out
+ * @size: size
+ * @neg: negative or not negative
+ * Return: the str'ized num
+*/
+
+char *decimal_to_string(digit *ptr_out, ssize_t size, int neg)
+{
+	;
+
+	digit rem, pow;
+	char *str, *p;
+	ssize_t strlen, i, j;
+
+	strlen = neg + 1 + (size - 1) * _PyLong_DECIMAL_SHIFT;
+	pow = 10;
+	rem = ptr_out[size - 1];
+	while (rem >= pow)
+	{
+		pow *= 10;
+		strlen++;
+	}
+	str = calloc(1, strlen + 1);
+	if (!str)
+		return (NULL);
+	p = str + strlen;
+	for (i = 0; i < size - 1; i++)
+	{
+		rem = ptr_out[i];
+		for (j = 0; j < _PyLong_DECIMAL_SHIFT; j++)
+		{
+			*--p = '0' + rem % 10;
+			rem /= 10;
+		}
+	}
+	rem = ptr_out[i];
+	do {
+		*--p = '0' + rem % 10;
+		rem /= 10;
+	} while (rem != 0);
+
+	if (neg)
+		*--p = '-';
+	return (str);
 }
