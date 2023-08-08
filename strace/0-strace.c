20c57fa12f96e054507db6de27dcd3fff4741180
HINKOKO <piotr62@live.fr>
2023-08-08 15:12:18 +0200

    freeing the output

--- strace/0-strace.c
+++ strace/0-strace.c
@@ -17,13 +17,11 @@ int main(int argc, char *argv[], char *envp[])
 	if (argc < 2)
 		return (fprintf(stderr, "Bad usage dude\n"), -1);
 
-	setbuf(stdout, NULL);
 	/* create child process */
 	pid = fork();
 	if (pid == 0)
 	{
 		ptrace(PTRACE_TRACEME, pid, NULL, NULL);
-		fclose(stdout);
 		execve(argv[1], &(argv[1]), envp);
 	}
 	else
