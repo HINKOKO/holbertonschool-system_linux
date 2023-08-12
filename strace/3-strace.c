502502a3a965a02741a2c585541ea38677c1c0d6
HINKOKO <piotr62@live.fr>
2023-08-12 16:11:47 +0200

    trying first version

--- strace/3-strace.c
+++ strace/3-strace.c
@@ -1,26 +1,14 @@
 #include "strace.h"
 #include "syscalls.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/ptrace.h>
-#include <sys/types.h>
-#include <sys/reg.h>
 
-int step_sys(pid_t child)
-{
-	int status;
-
-	while (1)
-	{
-		ptrace(PTRACE_SYSCALL, child, 0, 0);
-		waitpid(child, &status, 0);
-		if (WIFSTOPPED(status) && WSTOPSIG(status) == (SIGTRAP | 0x80))
-			return (0);
-		if (WIFEXITED(status))
-			break;
-	}
-	return (1);
-}
+/**
+ * print_regs - print the params from within registers
+ * which are on x86-64 ==> RDI, RSI, RDX, RCX, R8, R9
+ * @regs: struct for reading registers values
+ * @idx: index
+ * @reg: value of respective registers
+ * @str: string to print
+ */
 
 void print_regs(struct user_regs_struct regs)
 {
@@ -47,61 +35,53 @@ void print_regs(struct user_regs_struct regs)
 	}
 }
 
-int tracer(pid_t child)
-{
-	int status;
-	struct user_regs_struct regs;
-
-	waitpid(child, &status, 0);
-	ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_TRACESYSGOOD);
-	if (step_sys(child) != 0)
-		return (0);
-	ptrace(PTRACE_GETREGS, child, 0, &regs);
-	if (step_sys(child) != 0)
-		return (0);
-	ptrace(PTRACE_GETREGS, child, 0, &regs);
-	fprintf(stdout, " = %#lx\n", (unsigned long)regs.rax);
-	while (1)
-	{
-		if (step_sys(child) != 0)
-			break;
-		ptrace(PTRACE_GETREGS, child, 0, &regs);
-		fprintf(stdout, "%s(", syscalls_64[regs.orig_rax].name);
-		print_regs(regs);
-		if (step_sys(child) != 0)
-			break;
-		ptrace(PTRACE_GETREGS, child, 0, &regs);
-		fprintf(stdout, ") = %#lx\n", (unsigned long)regs.rax);
-	}
-	fprintf(stdout, ") = ?\n");
-	return (0);
-}
-
 /**
- *
- *
+ * main - executes an trace a given command in argv[1]
+ * @argc: argument count
+ * @argv: argument vector
+ * @envp: env string array
+ * Return: 0 success, -1 on failure
  */
 
 int main(int argc, char *argv[], char *envp[])
 {
+	int status = 0, flag = 0;
 	pid_t child;
+	struct user_regs_struct regs;
 
 	if (argc < 2)
-		return (fprintf(stderr, "%s [path_to_bin]", argv[0]));
+		return (fprintf(stderr, "%s [path_to_command]\n", argv[0]), -1);
 
+	setbuf(stdout, NULL);
 	child = fork();
-	++argv;
-	if (child == 0)
+	if (child < 0)
+		return (fprintf(stderr, "Failed to fork"), -1);
+	else if (child == 0)
 	{
-		ptrace(PTRACE_TRACEME);
+		ptrace(PTRACE_TRACEME, 0, 0, 0);
 		kill(getpid(), SIGSTOP);
-		return (execve(argv[0], argv, envp));
+		execve(argv[1], &(argv[1]), envp);
 	}
 	else
 	{
-		setbuf(stdout, NULL);
-		fprintf(stdout, "execve(%p, %p, %p)", (void *)argv[0],
-				(void *)argv, (void *)envp);
-		return (tracer(child));
+		wait(&status);
+		if (WIFEXITED(status))
+			return (0);
+		ptrace(PTRACE_SYSCALL, child, 0, 0);
+		while (wait(&status) && !WIFEXITED(status))
+		{
+			memset(&regs, 0, sizeof(regs));
+			ptrace(PTRACE_GETREGS, child, 0, &regs);
+			if (flag)
+				printf(") = %#lx\n", (unsigned long)regs.rax), flag = 0;
+			if (WSTOPSIG(status) == SIGTRAP && (long)regs.rax == -38)
+			{
+				printf("%s(", (char *)syscalls_64[regs.orig_rax].name), flag = 1;
+				print_regs(regs);
+			}
+			ptrace(PTRACE_SYSCALL, child, 0, 0);
+		}
+		printf(" = ?\n");
 	}
+	return (0);
 }
