ad1fb203ffe54f417d7928fabc6d9ba21501e386
HINKOKO <piotr62@live.fr>
2023-10-13 18:33:06 +0200

    hunting name's conflicts between functions

--- sockets/rest_api.h
+++ sockets/rest_api.h
@@ -71,7 +71,7 @@ typedef struct todo_info
 	todo_t *tail;
 } list_t;
 
-int start_server(void);
+int start_n_listen(void);
 int accept_msg(int sockfd);
 int send_response(int client_sd, char *response);
 int parse_response(char *raw_request, int client_fd);
